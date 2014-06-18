4505f0867a850919f7fd58299195dba9c504c83c
Marco Jahn <marco.jahn@gmail.com>
2014-06-18 09:41:24 +0200

    moved

--- app/view/login/LoginModel.js
+++
@@ -1,4 +0,0 @@
-Ext.define('Ares.view.login.LoginModel', {
-    extend: 'Ext.app.ViewModel',
-    alias: 'viewmodel.login'
-});
--- app/view/main/MainModel.js
+++
@@ -1,8 +0,0 @@
-Ext.define('Ares.view.main.MainModel', {
-    extend: 'Ext.app.ViewModel',
-    alias: 'viewmodel.main'
-
-    /**
-     * @property {Ticket.model.User} currentUser
-     */
-});
